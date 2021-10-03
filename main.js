ce8f27467c35d7c5ccaa34cee0669c2fc3ee4d7f
azael537 <azaelolivargarcia@gmail.com>
2021-10-03 16:00:09 -0500

    Se suben los dos primeros problemas, Concluir ejercicio 3 y 4

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//Ejericios de Javascript
+
+/*
+1. Crea una función llamada palindromo() que reciba como parámetro una palabra, si la oración es un palíndromo la función deberá devolver true en otro caso, devolver false
+Extra: con oraciones
+*/
 // R E C O N O C E R
 
 function palindromo(palabra) {
@@ -11,6 +17,13 @@ function palindromo(palabra) {
     }
 }
 
+/*
+2. Crea una función llamada longestCountry(), esta función recibirá de parámetro un arreglo con nombres de distintos países, la función deberá retornar el país con mayor número de letras. 
+Toma como ejemplo, del siguiente arreglo de países:
+
+const paises = ["México", "Colombia", "Peru", "El Salvador"]
+*/
+
 const paises = ["México", "Colombia", "Peru", "El Salvador"];
 function paisMasLargo(arregloDePaises) {
     let paisLargo = "";
@@ -21,3 +34,15 @@ function paisMasLargo(arregloDePaises) {
     }
     return paisLargo;
 }
+
+
+/*
+3. Crea una función llamada farenheitToCelsius() que reciba como parámetro los grados farenheit y los convierta a grados centígrados.
+*/
+
+
+
+
+/*
+4. Crea una función que imprima el número de parámetros enviados.
+*/
